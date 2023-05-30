f842381312891a9817cec0866280d8065159938a
SCL3 <106513368+SCL3@users.noreply.github.com>
2023-05-31 01:06:44 +0200

    Add files via upload

--- inscription_jeune.php
+++ inscription_jeune.php
@@ -1,5 +1,19 @@
 <?php
-	echo "Ca marche pas";
-	$test = $_REQUEST["q"];
-	echo "<br>" + $test;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Vérifier si le prénom a été envoyé
+  if (isset($_POST["prenom"])) {
+    $prenom = $_POST["prenom"];
+    
+    // Faire quelque chose avec le prénom
+    // Par exemple, l'afficher
+    echo "Prénom : " . $prenom ."<br>";
+  }
+  if (isset($_POST["nom"])) {
+    $nom = $_POST["nom"];
+    
+    // Faire quelque chose avec le prénom
+    // Par exemple, l'afficher
+    echo "Nom : " . $nom;
+  }
+}
 ?>
