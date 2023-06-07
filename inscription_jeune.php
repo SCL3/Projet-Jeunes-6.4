77b010feb252b9f1287f338c1cbe44ab95639e6b
SCL3 <106513368+SCL3@users.noreply.github.com>
2023-06-07 22:17:33 +0200

    Add files via upload

--- inscription_jeune.php
+++ inscription_jeune.php
@@ -1,5 +1,13 @@
 <?php
 
+// Fonction pour calculer l'âge à partir de la date de naissance
+function calculateAge($date) {
+  $birthDate = new DateTime($date);
+  $today = new DateTime();
+  $age = $today->diff($birthDate)->y;
+  return $age;
+}
+
 function test(string $prenom, string $nom, string $email, string $date, string $mdp1, string $mdp2){
   // Fonction qui vérifie toute les données en fonction de chaques conditions
   
@@ -69,7 +77,18 @@ function test(string $prenom, string $nom, string $email, string $date, string $
     return 0;
   } 
 
-  // On vérifie l'âge (ENTRE 16 et 30)
+  // Vérification de l'âge entre 14 et 30 ans
+  $age = calculateAge($date); // Appel d'une fonction pour calculer l'âge à partir de la date de naissance
+  if ($age < 14) {
+    echo "Vous êtes trop jeune pour vous inscrire.";
+    return 0;
+  }
+  if ($age > 30){
+    echo "Vous êtes trop vieux pour vous inscrire.";
+    return 0;
+  }
+
+  /*// On vérifie l'âge (ENTRE 16 et 30)
   $temp = strtotime($date);
   $annee = date('Y',$temp);
   if ($annee < 1993){ // Ici, le "jeune" a plus de 30 ans...
@@ -79,7 +98,7 @@ function test(string $prenom, string $nom, string $email, string $date, string $
   if($annee >= date('Y')){  // Compare sa date de naissance avec l'année actuelle
     echo "Tu viens du futur ?? :O";
     return 0;
-  }
+  }*/
   
   return 1;
 }
@@ -97,75 +116,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {  // Si la requête est bien reçu
     $date = $_POST["date"];
     $mdp1 = $_POST["mdp1"];
     $mdp2 = $_POST["mdp2"];
-    
-    $age = calculateAge($date); // Appel d'une fonction pour calculer l'âge à partir de la date de naissance
-    
-    if ($age >= 14 && $age <= 30) { // Vérification de l'âge entre 14 et 30 ans
-      if(test($prenom, $nom, $email, $date, $mdp1, $mdp2) == 1){
-        // Connexion à la base de données SQLite
-        $database = new PDO('sqlite:users.db');
-        // Création de la table si elle n'existe pas déjà
-        $database->exec('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, prenom TEXT, nom TEXT, email TEXT, date_naissance TEXT, mdp TEXT)');
-        // Insertion des données dans la table
-        $database->exec("INSERT INTO users (prenom, nom, email, date_naissance, mdp) VALUES ('$prenom', '$nom', '$email', '$date', '$mdp1')");
-       
-        // Envoi du mail de confirmation
-        
-        require_once 'envoi_email.php'; // Inclure le fichier d'envoi d'e-mail
-        
-        $subject = "Confirmation d'inscription";
-        $message = "Bonjour $prenom,\n\nMerci de vous être inscrit sur notre site.\n\nVeuillez cliquer sur le lien suivant pour confirmer votre adresse e-mail : [lien de confirmation].\n\nCordialement,\nL'équipe du site";
-        $headers = "From: no-reply@monsite.com";
-        // mail($email, $subject, $message, $headers);
-        
-        echo "Création du compte. Un email de confirmation a été envoyé à votre adresse.";
-        // Autres actions à effectuer pour l'inscription réussie
+
+    if(test($prenom, $nom, $email, $date, $mdp1, $mdp2) == 1){
+      // Connexion à la base de données SQLite
+      $database = new PDO('sqlite:users.db');
+      // Création de la table si elle n'existe pas déjà
+      $database->exec('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, prenom TEXT, nom TEXT, email TEXT, date_naissance TEXT, mdp TEXT)');
+      // Insertion des données dans la table
+      $database->exec("INSERT INTO users (prenom, nom, email, date_naissance, mdp) VALUES ('$prenom', '$nom', '$email', '$date', '$mdp1')");
+      
+      // Envoi du mail de confirmation
+      
+      require_once 'envoi_email.php'; // Inclure le fichier d'envoi d'e-mail
+      
+      $subject = "Confirmation d'inscription";
+      $message = "Bonjour $prenom,\n\nMerci de vous être inscrit sur notre site.\n\nVeuillez cliquer sur le lien suivant pour confirmer votre adresse e-mail : [lien de confirmation].\n\nCordialement,\nL'équipe du site";
+      $headers = "From: no-reply@monsite.com";
+      // mail($email, $subject, $message, $headers);
+      
+      echo "Création du compte. Un email de confirmation a été envoyé à votre adresse.";
+      // Autres actions à effectuer pour l'inscription réussie
       }
-    } elseif ($age < 14) {
-      echo "Vous êtes trop jeune pour vous inscrire.";
-    } else {
-      echo "Vous êtes trop vieux pour vous inscrire.";
-    }
+     
   }
 }
-
-// Fonction pour calculer l'âge à partir de la date de naissance
-function calculateAge($date) {
-  $birthDate = new DateTime($date);
-  $today = new DateTime();
-  $age = $today->diff($birthDate)->y;
-  return $age;
-}
-
-  
-
- 
- 
-  /*if (isset($_POST["prenom"])) {
-    $prenom = $_POST["prenom"];
-  }
-  else{
-    echo "Veuillez inscrire votre prénom.";
-  }
-  else if (isset($_POST["nom"])) {
-    $nom = $_POST["nom"];
-    echo "Nom : " . $nom . "<br>";
-  }
-  else if (isset($_POST["email"])) {
-    $email = $_POST["email"];
-    echo "Email : " . $email ."<br>";
-  }
-  if (isset($_POST["date"])) {
-     $email = $_POST["email"];
-    echo "Date : " . $date ."<br>";
-  }
-  if (isset($_POST["mdp1"])) {
-    $email = $_POST["email"];
-    echo "Mdp1 : " . $mdp1 ."<br>";
-  }
-  if (isset($_POST["mdp2"])) {
-    $mdp2 = $_POST["mdp2"];
-    echo "Mdp2 : " . $mdp2 ."<br>";
-  }*/
-
 ?>
