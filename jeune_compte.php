70fd3c8c009ee9774adbd73c27f3c69a61e04a5c
Mohamed <106923294+Mogramed@users.noreply.github.com>
2023-06-11 00:30:50 +0200

    Update jeune_compte.php

--- jeune_compte.php
+++ jeune_compte.php
@@ -97,28 +97,80 @@ if (!isset($_SESSION['connecte']) || $_SESSION['connecte'] !== true) {
 		<button onclick="afficheDiv(4)">Envoyer référence au consultant</button>
 		<button onclick="afficheDiv(5)">Inclure référence validées dans CV</button><br>
 		<div id="div1" class="jeune_compte_div">
-			<p>Modifier le profil</p>
-			<form class="profil">
-				<!-- Les champs du formulaire de modification du profil -->
-				<label>Prénom : </label>
-				<input type="text" name ="prenom" placeholder="Prénom" required/> <br>
-				<label>Nom : </label>
-				<input type="text" name ="nom" placeholder="Nom" required/> <br>
-				<label>Email : </label>
-				<input type="email" name ="email" placeholder="Email" required/> <br>
-				<label> Date de naissance</label>
-				<input type="date" name ="date" placeholder="" required/> <br>
-				<label>Ancien mot de Passe</label>
-				<input type="password" name ="mdpold" placeholder="Mot de passe" required/> <br>
-				<label>Nouveau mot de Passe</label>
-				<input type="password" name ="mdp1" placeholder="Confirmez le mot de passe" required/> <br>
-				<label>Confirmer mot de passe</label>
-				<input type="password" name ="mdp2" placeholder="Confirmez le mot de passe" required/> <br>
-				<p id="texte1"> </p>
-				<input type="button" name="modifier" value="Enregistrer les modifications" onclick="modifier()"/>
+	<p>Modifier le profil</p>
+	<form class="profil" method="post" action="">
+		<!-- Les champs du formulaire de modification du profil -->
+		<label>Prénom : </label>
+		<input type="text" name="prenom" placeholder="Prénom" required/> <br>
+		<label>Nom : </label>
+		<input type="text" name="nom" placeholder="Nom" required/> <br>
+		<label>Email : </label>
+		<input type="email" name="email" placeholder="Email" required/> <br>
+		<label>Date de naissance</label>
+		<input type="date" name="date" placeholder="" required/> <br>
+		<label>Ancien mot de Passe</label>
+		<input type="password" name="mdpold" placeholder="Mot de passe" required/> <br>
+		<label>Nouveau mot de Passe</label>
+		<input type="password" name="mdp1" placeholder="Confirmez le mot de passe" required/> <br>
+		<label>Confirmer mot de passe</label>
+		<input type="password" name="mdp2" placeholder="Confirmez le mot de passe" required/> <br>
+		<p id="texte1"> </p>
+		<input type="submit" name="modifier" value="Enregistrer les modifications"/>
+	</form>
+</div>
 
-			</form>
-		</div>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// Vérifier si toutes les données ont été envoyées
+	if (isset($_POST["prenom"], $_POST["nom"], $_POST["email"], $_POST["date"], $_POST["mdpold"], $_POST["mdp1"], $_POST["mdp2"])) {
+		// Récupération de toutes les données
+		$prenom = $_POST["prenom"];
+		$nom = $_POST["nom"];
+		$email = $_POST["email"];
+		$date = $_POST["date"];
+		$mdpold = $_POST["mdpold"];
+		$mdp1 = $_POST["mdp1"];
+		$mdp2 = $_POST["mdp2"];
+
+		// Appel de la fonction de validation
+		$validation = validerInformations($prenom, $nom, $email, $date, $mdp1, $mdp2);
+
+		if ($validation === 0) {
+    echo "Erreur de validation";
+} else {
+    // Connexion à la base de données (vous devez remplacer les valeurs par les vôtres)
+    $servername = "localhost";
+    $username = "nom_utilisateur";
+    $password = "mot_de_passe";
+    $dbname = "nom_base_de_donnees";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Erreur de connexion à la base de données : " . $conn->connect_error);
+    }
+
+    // Échapper les valeurs pour éviter les injections SQL
+    $prenom = $conn->real_escape_string($prenom);
+    $nom = $conn->real_escape_string($nom);
+    $email = $conn->real_escape_string($email);
+    $date = $conn->real_escape_string($date);
+    $mdp1_hash = password_hash($mdp1, PASSWORD_DEFAULT); // Hashage du nouveau mot de passe
+
+    // Construire et exécuter la requête SQL pour mettre à jour les informations du profil
+    $sql = "UPDATE user SET prenom='$prenom', nom='$nom', email='$email', date='$date', mot_de_passe='$mdp1_hash' WHERE id_utilisateur='$id_utilisateur'";
+    if ($conn->query($sql) === true) {
+        echo "Modifications enregistrées avec succès";
+    } else {
+        echo "Erreur lors de l'enregistrement des modifications : " . $conn->error;
+    }
+
+    // Fermer la connexion à la base de données
+    $conn->close();
+  }
+
+}
+?>
+
 		<div id="div2" class="jeune_compte_div">
 			<p>Créer une demande de référence</p>
 			<form method="post" action="creer_reference.php">
